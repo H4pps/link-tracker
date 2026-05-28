41c68fc06b0f242266aa5ae85ded0f4349466c0c
H4pps <juriy.bulanov@gmail.com>
2026-05-28 13:41:18 +0200

    feat(db/orm): add entity classes

---
+++ scrapper/src/main/java/backend/academy/linktracker/scrapper/infrastructure/memory/orm/entity/ChatEntity.java
@@ -0,0 +1,30 @@
+package backend.academy.linktracker.scrapper.infrastructure.memory.orm.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "chats")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChatEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "chat_id", nullable = false, unique = true)
+    private Long chatId;
+
+    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    private Instant createdAt;
+}
