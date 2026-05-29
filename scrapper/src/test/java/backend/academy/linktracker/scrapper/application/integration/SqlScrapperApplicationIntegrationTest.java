d4f43db6789298c25db4e15cd09ee765877740e3
H4pps <juriy.bulanov@gmail.com>
2026-05-29 09:29:38 -0400

    test(db): add orm and sql integration tests

---
+++ scrapper/src/test/java/backend/academy/linktracker/scrapper/application/integration/SqlScrapperApplicationIntegrationTest.java
@@ -0,0 +1,25 @@
+package backend.academy.linktracker.scrapper.application.integration;
+
+import backend.academy.linktracker.scrapper.ScrapperApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+        classes = ScrapperApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = {
+            "server.port=0",
+            "app.grpc.server.port=0",
+            "app.scheduler.enabled=false",
+            "app.scheduler.link-page-size=1",
+            "app.database.access-type=SQL",
+            "app.github.token=test-github-token",
+            "app.stackoverflow.key=test-stackoverflow-key",
+            "app.stackoverflow.access-token=test-stackoverflow-access-token"
+        })
+class SqlScrapperApplicationIntegrationTest extends AbstractScrapperApplicationIntegrationTest {
+
+    @Override
+    protected String accessType() {
+        return "SQL";
+    }
+}
