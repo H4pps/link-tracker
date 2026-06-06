a3207376e0f2bbc0bf12d11d91147b49b96a8007
H4pps <juriy.bulanov@gmail.com>
2026-06-06 15:20:28 +0200

    refactor(scrapper): move update type labels into enum

--- scrapper/src/main/java/backend/academy/linktracker/scrapper/application/external/update/ExternalUpdateType.java
+++ scrapper/src/main/java/backend/academy/linktracker/scrapper/application/external/update/ExternalUpdateType.java
@@ -4,8 +4,18 @@ package backend.academy.linktracker.scrapper.application.external.update;
  * Supported external update kinds tracked by scrapper.
  */
 public enum ExternalUpdateType {
-    GITHUB_ISSUE,
-    GITHUB_PULL_REQUEST,
-    STACKOVERFLOW_ANSWER,
-    STACKOVERFLOW_COMMENT
+    GITHUB_ISSUE("GitHub Issue"),
+    GITHUB_PULL_REQUEST("GitHub Pull Request"),
+    STACKOVERFLOW_ANSWER("StackOverflow Answer"),
+    STACKOVERFLOW_COMMENT("StackOverflow Comment");
+
+    private final String displayLabel;
+
+    ExternalUpdateType(String displayLabel) {
+        this.displayLabel = displayLabel;
+    }
+
+    public String displayLabel() {
+        return displayLabel;
+    }
 }
